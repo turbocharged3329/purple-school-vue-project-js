ed78f1345eaebdd985af8f71d9ca97326756dded
kitaev_v <kitaev@reezon.ly>
2025-07-02 17:35:09 +0300

    Добавлен компонент и стили для кнопки соханения

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
-
+import WButton from "./components/WButton.vue";
 </script>
 
 <template>
-
+<main class="w-main-container">
+  <WButton />
+</main>
 </template>
 
 <style scoped>
-
+.w-main-container {
+  width: 67.5vw;
+  background-color: var(--color-bg-main);
+  box-sizing: border-box;
+  padding: 3.5rem 3.125rem 3.875rem; /* 56px 50px 62px */
+  border-radius: 1.5625rem; /* 25px */
+}
 </style>
---
+++ src/components/WButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+<button class="w-button">Сохранить</button>
+</template>
+
+<style scoped>
+.w-button:hover {
+  background: var(--btn-bg-gradient--inverted);
+}
+
+.w-button {
+  border: none;
+  border-radius: 10px;
+  background: var(--btn-bg-gradient);
+  color: var(--color-primary);
+  font-size: 20px;
+  font-weight: 600;
+  padding: 14px;
+}
+</style>
